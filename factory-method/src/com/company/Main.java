d8533ea49044ca85fcb1b673efcb2f3ddac0ce22
Håkon Schia <hakonschia5@gmail.com>
2019-11-11 16:27:38 +0100

    Minor formating changes

--- factory-method/src/com/company/Main.java
+++ factory-method/src/com/company/Main.java
@@ -7,7 +7,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-
         System.out.println("Write C for new car, B for new boat, M for new motorcycle");
         String input = s.next();
 
